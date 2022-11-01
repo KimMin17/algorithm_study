6c52f995cea26e4ee8ef89a4b91e4f74d89f142d
Kim Min <kimmin17@sogang.ac.kr>
2022-11-02 01:06:58 +0900

    update

--- Baekjoon/7576.py
+++ Baekjoon/7576.py
@@ -1,5 +1,5 @@
 import sys
-import deque
+from collections import deque
 
 m, n = map(int, sys.stdin.readline().split())
 
@@ -13,10 +13,12 @@ dy = [1, 0, -1, 0]
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 1:
-            ripe_tomas.append((i, j))
+            ripe_tomas.append((i, j, 0))
 
+max_step = 0
 while ripe_tomas:
-    cur_x, cur_y = ripe_tomas.popleft()
+    cur_x, cur_y, cur_step = ripe_tomas.popleft()
+    max_step = max(max_step, cur_step)
     for i in range(4):
         new_x = cur_x + dx[i]
         new_y = cur_y + dy[i]
@@ -24,4 +26,12 @@ while ripe_tomas:
         if 0 > new_x or new_x >= n or 0 > new_y or new_y >= m: continue
 
         if arr[new_x][new_y] == 0:
-            arr[new_x][new_y] = 0
+            arr[new_x][new_y] = 1
+            ripe_tomas.append((new_x, new_y, cur_step + 1))
+
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 0:
+            max_step = -1
+
+print(max_step)
