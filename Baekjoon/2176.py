d2ffcba7af2d95a0412dab94deb8e516147c8bfc
Kim Min <kimmin17@sogang.ac.kr>
2023-01-20 01:53:21 +0900

    update

--- Baekjoon/2176.py
+++ Baekjoon/2176.py
@@ -1,3 +1,4 @@
+'''
 from queue import PriorityQueue
 
 n, m = map(int, input().split())
@@ -13,7 +14,7 @@ for _ in range(m):
     graph[v2][v1] = d
 
 for i in range(n):
-    graph[n][n] = 0
+    graph[i][i] = 0
 
 start, dest = 1, 2
 
@@ -41,20 +42,56 @@ def dijkstra(start):
 
     return dist
 
-dist = dijkstra(dest)
+'''
+
+import sys
+import heapq
+
+limit_number = 200000
+sys.setrecursionlimit(limit_number)
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+nodes = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, c = map(int, sys.stdin.readline().rstrip().split())
+    nodes[a].append([b, c])
+    nodes[b].append([a, c])
+INF = sys.maxsize
+def Dijsktra(start):
+    distances = [INF for _ in range(n+1)]
+    distances[start] = 0
+
+    pq = []
+    heapq.heappush(pq, [0, start])
+
+    while pq:
+        cur_cost, cur_node = heapq.heappop(pq)
+
+        if distances[cur_node] < cur_cost: continue
+
+        for next_node, next_cost in nodes[cur_node]:
+            if distances[next_node] > cur_cost + next_cost:
+                distances[next_node] = cur_cost + next_cost
+                heapq.heappush(pq, [cur_cost+next_cost, next_node])
+    return distances
+
+dist = Dijsktra(2)
+
+start, dest = 1, 2
+
 start_cost = dist[start]
 
-dp = [[-1] * n for _ in range(n)]
+dp = [-1] * (n+1)
 
-def dfs(start, dest):
-    if dp[start][dest] != -1: return dp[start][dest]
-    if start == dest: return 1
+def dfs(start):
+    if dp[start] != -1: return dp[start]
     
     result = 0
-    for i in range(n):
-        if graph[start][i] != INF:
-            result += dfs(i, dest)
-    dp[start][dest] = result
+    for node, node_dist in nodes[start]:
+        if dist[node] < start_cost:
+            result += dfs(node)
+    dp[start] = result
     return result
 
-print(dfs(start, dest))
+dp[dest] = 1
+print(dfs(start))
